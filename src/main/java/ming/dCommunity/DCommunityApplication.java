21e7cffcd84cedbc4c281464bc2de16409872502
6uiwj <oterges00@gmail.com>
2025-01-20 17:13:38 +0900

    250120 PloD project created

---
+++ src/main/java/ming/dCommunity/DCommunityApplication.java
@@ -0,0 +1,13 @@
+package ming.dCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ming/dCommunity/DCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package ming.dCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
