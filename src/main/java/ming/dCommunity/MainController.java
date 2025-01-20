387fed05e62b9c879aac09b78fa44a735dcd6a68
6uiwj <oterges00@gmail.com>
2025-01-20 17:45:31 +0900

    250120 - create main controller

--- src/main/java/ming/dCommunity/MainController.java
+++ src/main/java/ming/dCommunity/MainController.java
@@ -1,4 +1,15 @@
 package ming.dCommunity;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MainController {
+    @GetMapping("/index")
+    @ResponseBody
+    public String index() {
+        return "main";
+    }
 }
